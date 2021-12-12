e1011fe523f8b5237cc0bcfb632fa4b4f612fe71
tanshengjunjun <1002188561@qq.com>
2021-12-12 12:07:19 +0800

    添加@Aspect(@Before)、原来和junit区分开的

--- src/test/java/com/example/demo/Demo3ApplicationTests.java
+++ src/test/java/com/example/demo/Demo3ApplicationTests.java
@@ -1,14 +1,28 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+//@SpringBootTest
 class Demo3ApplicationTests {
 
+    @BeforeEach
+    public void s(){
+        System.out.println("before?");
+    }
     @Test
     void contextLoads() {
         System.out.println("123test");
+
     }
 
 }
